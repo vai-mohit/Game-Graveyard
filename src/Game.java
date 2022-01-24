60473c14c28f95bc68ccb16e702f63242cca0e60
vai-mohit <mohit.vaishnav_cs.ccv19@gla.ac.in>
2022-01-24 14:21:36 +0530

    My 1st Project

---
+++ src/Game.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mohit Vaishnav
+ */
+public class Game {
+ public static String email=null;
+ public static String username=null;
+  public static String password=null;  
+}
